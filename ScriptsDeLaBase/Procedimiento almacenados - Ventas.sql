e10287828d73510e22c66bd643fce8868402a1eb
Lautaroinv <romeroml193@gmail.com>
2023-10-17 11:29:45 -0300

    update Scripts

--- ScriptsDeLaBase/Procedimiento almacenados - Ventas.sql
+++ ScriptsDeLaBase/Procedimiento almacenados - Ventas.sql
@@ -3,11 +3,12 @@
 
 CREATE PROCEDURE SP_ProductosMasVendidos
 AS
-select TOP 5 producto.marca_producto+' - '+ producto.descripcion_producto as Producto, count(detalle_venta.id_producto) as Cantidad_vendida
+select TOP 5 Marca.Nombre+' - '+ producto.descripcion_producto as Producto, count(detalle_venta.id_producto) as Cantidad_vendida
 	from detalle_venta
 	inner join producto on detalle_venta.id_producto = producto.id_producto
 	inner join categoria on categoria.id_categoria = producto.id_categoria
-	group by detalle_venta.id_producto, producto.marca_producto, producto.descripcion_producto
+	inner join marca on marca.Id_Marca = producto.Id_Marca
+	group by detalle_venta.id_producto, Marca.Nombre, producto.descripcion_producto
 	order by Cantidad_vendida DESC
 GO
 
@@ -15,11 +16,11 @@ GO
 
 CREATE PROCEDURE SP_CategoriasMasVendidas
 AS
-	select categoria.descripcion_categoria, COUNT(categoria.id_categoria) AS CategoriaMasVendida 
+	select categoria.nombre_categoria, COUNT(categoria.id_categoria) AS CategoriaMasVendida 
 		from detalle_venta
 		inner join producto ON detalle_venta.id_producto = producto.id_producto
 		inner join categoria ON producto.id_categoria = categoria.id_categoria
-		group by producto.id_categoria, categoria.descripcion_categoria
+		group by producto.id_categoria, categoria.nombre_categoria
 		order by CategoriaMasVendida DESC
 GO
 
@@ -63,7 +64,7 @@ AS
 SELECT venta.id_venta AS ID_Venta, 
 	   venta.venta_fecha AS Fecha,
 	   persona.apellido+' '+persona.nombre AS Cliente, 
-	   metodo_pago.descripcion_metodo_pago AS Metodo_pago,
+	   metodo_pago.nombre_metodo_pago AS Metodo_pago,
 	   usuario.email AS Vendedor
 FROM venta
 
